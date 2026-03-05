71617bfea4b5b317f3655ed85b92e7f1b0fc5afd
thenthlife <albertosancheznino@gmail.com>
2026-03-05 17:22:37 +0100

    Update app.js

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ If something important happened, summarize it in one sentence.
 `;
 
   const response = await fetch(
-    "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=" + API_KEY,
+    ""https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=" + API_KEY,
     {
       method: "POST",
       headers: { "Content-Type": "application/json" },
